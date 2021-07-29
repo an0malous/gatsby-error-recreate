cc287c67224cfa1dd3f22436b8504d23facafbda
Daniel Brown <daniel-brown@digifusion.dev>
2021-07-30 06:54:05 +0900

    added withQuerySnapshot

--- src/components/testComponent.test.tsx
+++ src/components/testComponent.test.tsx
@@ -2,11 +2,12 @@ import React from "react"
 
 import TestComponent from "./testComponent"
 import { render, screen } from "@testing-library/react"
+import { withQuerySnapshot } from "gatsby-plugin-testing"
 
 describe("test", ()=> {
-   it('Test test component', ()=> {
+   it('Test test component', withQuerySnapshot(()=> {
       render(<TestComponent name="Owen" />)
       const name = screen.getByText(/owen/i)
       expect(name).toBeInTheDocument()
-   })
+   }))
 })
